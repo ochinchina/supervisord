71207e35adcb08ab0465142e309c73353c22a89d
Alexander Vasilev <avasilev@abtronics.ru>
2017-08-03 13:21:37 +0300

    implemented "status", "start" and "stop" commands

--- ctl.go
+++ ctl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type CtlCommand struct {
@@ -12,7 +13,62 @@ type CtlCommand struct {
 var ctlCommand CtlCommand
 
 func (x *CtlCommand) Execute(args []string) error {
-	fmt.Printf("empty ctl command")
+	if len(args) == 0 {
+		return nil
+	}
+
+	rpcc := NewXmlRPCClient(x.Host, x.Port)
+
+	verb, processes := args[0], args[1:]
+	has_processes := len(processes) > 0
+	processes_map := make(map[string]bool)
+	for _, process := range processes {
+		processes_map[strings.ToLower(process)] = true
+	}
+
+	switch verb {
+
+	////////////////////////////////////////////////////////////////////////////////
+	// STATUS
+	////////////////////////////////////////////////////////////////////////////////
+	case "status":
+		if reply, err := rpcc.GetAllProcessInfo(); err == nil {
+			for _, pinfo := range reply.Value {
+				name := strings.ToLower(pinfo.Name)
+				description := pinfo.Description
+				if strings.ToLower(description) == "<string></string>" {
+					description = ""
+				}
+				if !has_processes || processes_map[name] {
+					fmt.Printf("%-33s%-10s%s\n", name, pinfo.Statename, description)
+				}
+			}
+		}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// START or STOP
+	////////////////////////////////////////////////////////////////////////////////
+	case "start", "stop":
+		state := map[string]string{
+			"start": "started",
+			"stop":  "stopped",
+		}
+		for _, pname := range processes {
+			if reply, err := rpcc.ChangeProcessState(verb, pname); err == nil {
+				fmt.Printf("%s: ", pname)
+				if !reply.Value {
+					fmt.Printf("not ")
+				}
+				fmt.Printf("%s\n", state[verb])
+			} else {
+				fmt.Printf("%s: failed [%v]\n", pname, err)
+			}
+		}
+
+	default:
+		fmt.Println("unknown command")
+	}
+
 	return nil
 }
 
