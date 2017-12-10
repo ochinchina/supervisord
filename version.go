1159a34ff4c8e7edbfaeeb8d6101a0e873a8162c
Ou Changhua <ochinchina@gmail.com>
2017-12-10 01:52:50 +0000

    add version support

---
+++ version.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+const VERSION = "1.0.001"
+
+type VersionCommand struct {
+}
+
+var versionCommand VersionCommand
+
+func (v VersionCommand) Execute(args []string) error {
+	fmt.Println(VERSION)
+	return nil
+}
+
+func init() {
+	parser.AddCommand("version",
+		"show the version of supervisor",
+		"display the supervisor version",
+		&versionCommand)
+}
