f3cd839cb0f69d6ff0e4a17795e81100e42640e9
Chang Hua Ou <steven.ou@nokia.com>
2018-03-10 03:35:10 -0500

    support environment variable file

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/signal"
 	"runtime"
+	"strings"
 	"syscall"
+	"unicode"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/jessevdk/go-flags"
+	log "github.com/sirupsen/logrus"
 )
 
 type Options struct {
 	Configuration string `short:"c" long:"configuration" description:"the configuration file" default:"supervisord.conf"`
 	Daemon        bool   `short:"d" long:"daemon" description:"run as daemon"`
+	EnvFile       string `long:"env-file" description:"the environment file"`
 }
 
 func init() {
@@ -41,8 +45,36 @@ func initSignals(s *Supervisor) {
 var options Options
 var parser = flags.NewParser(&options, flags.Default & ^flags.PrintErrors)
 
+func LoadEnvFile() {
+	f, err := os.Open(options.EnvFile)
+	if err != nil {
+		log.WithFields(log.Fields{"file": options.EnvFile}).Error("Fail to open environment file")
+		return
+	}
+	defer f.Close()
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if strings.HasPrefix(line, "export") && len(line) > len("export") && unicode.IsSpace(rune(line[len("export")])) {
+			line = strings.TrimSpace(line[len("export"):])
+		}
+		pos := strings.Index(line, "=")
+		if pos != -1 {
+			k := strings.TrimSpace(line[0:pos])
+			v := strings.TrimSpace(line[pos+1:])
+			if len(k) > 0 && len(v) > 0 {
+				os.Setenv(k, v)
+			}
+		}
+	}
+}
+
 func RunServer() {
 	// infinite loop for handling Restart ('reload' command)
+	LoadEnvFile()
 	for true {
 		s := NewSupervisor(options.Configuration)
 		initSignals(s)
--- version.go
+++ version.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-const VERSION = "1.0.010"
+const VERSION = "1.0.011"
 
 type VersionCommand struct {
 }
