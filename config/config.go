b6ca329d3120306356e24d66e4ecb2b833af487d
AlexStocks <alexstocks@foxmail.com>
2019-01-16 20:55:41 +0800

    Fix: remove program from config just by its program name without 'program:' prefix'

--- config/config.go
+++ config/config.go
@@ -547,6 +547,6 @@ func (c *Config) String() string {
 }
 
 func (c *Config) RemoveProgram(programName string) {
-	delete(c.entries, fmt.Sprintf("program:%s", programName))
+	delete(c.entries, programName)
 	c.ProgramGroup.Remove(programName)
 }
