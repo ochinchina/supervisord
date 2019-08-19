d9156733270854c81b701c8cb5749324f8fe9589
stou <steven.ou@nokia.com>
2019-08-19 06:01:33 +0000

    Feature request: add file watcher for automatic reload replased application code. #142

--- go.mod
+++ go.mod
@@ -1,19 +1,24 @@
 module supervisord
 
 require (
-	github.com/GeertJohan/go.rice v0.0.0-20170420135705-c02ca9a983da
+	github.com/GeertJohan/go.rice v1.0.0
 	github.com/daaku/go.zipexe v1.0.1 // indirect
-	github.com/gorilla/mux v1.7.1
-	github.com/gorilla/rpc v1.1.0
-	github.com/jessevdk/go-flags v1.3.0
-	github.com/kardianos/osext v0.0.0-20170510131534-ae77be60afb1 // indirect
+	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/rpc v1.2.0
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/nkovacs/streamquote v1.0.0 // indirect
+	github.com/ochinchina/filechangemonitor v0.3.1 // indirect
 	github.com/ochinchina/go-daemon v0.1.5
 	github.com/ochinchina/go-ini v1.0.1
 	github.com/ochinchina/go-reaper v0.0.0-20181016012355-6b11389e79fc
 	github.com/ochinchina/gorilla-xmlrpc v0.0.0-20171012055324-ecf2fe693a2c
 	github.com/ochinchina/supervisord v0.6.3
-	github.com/rogpeppe/go-charset v0.0.0-20180617210344-2471d30d28b4 // indirect
-	github.com/sirupsen/logrus v0.0.0-20170713114250-a3f95b5c4235
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20170814044513-c84c1ab9fd18 // indirect
+	github.com/rogpeppe/go-charset v0.0.0-20190617161244-0dc95cdf6f31 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- process/process.go
+++ process/process.go
@@ -2,6 +2,7 @@ package process
 
 import (
 	"fmt"
+	"github.com/ochinchina/filechangemonitor"
 	"github.com/ochinchina/supervisord/config"
 	"github.com/ochinchina/supervisord/events"
 	"github.com/ochinchina/supervisord/logger"
@@ -358,6 +359,7 @@ func (p *Process) createProgramCommand() error {
 		log.WithFields(log.Fields{"user": p.config.GetString("user", "")}).Error("fail to run as user")
 		return fmt.Errorf("fail to set user")
 	}
+	p.setProgramRestartChangeMonitor(args[0])
 	set_deathsig(p.cmd.SysProcAttr)
 	p.setEnv()
 	p.setDir()
@@ -365,6 +367,29 @@ func (p *Process) createProgramCommand() error {
 
 	p.stdin, _ = p.cmd.StdinPipe()
 	return nil
+
+}
+
+func (p *Process) setProgramRestartChangeMonitor(programPath string) {
+	if p.config.GetBool("restart_when_binary_changed", false) {
+		AddProgramChangeMonitor(programPath, func(path string, mode filechangemonitor.FileChangeMode) {
+			log.WithFields(log.Fields{"program": p.GetName()}).Info("program is changed, resatrt it")
+			p.Stop(true)
+			p.Start(true)
+		})
+	}
+	dir_monitor := p.config.GetString("restart_directory_monitor", "")
+	file_pattern := p.config.GetString("restart_file_pattern", "*")
+	if dir_monitor != "" {
+		AddConfigChangeMonitor(dir_monitor, file_pattern, func(path string, mode filechangemonitor.FileChangeMode) {
+            //fmt.Printf( "file_pattern=%s, base=%s\n", file_pattern, filepath.Base( path ) )
+            //if matched, err := filepath.Match( file_pattern, filepath.Base( path ) ); matched && err == nil {
+			log.WithFields(log.Fields{"program": p.GetName()}).Info("configure file for program is changed, resatrt it")
+			p.Stop(true)
+			p.Start(true)
+            //}
+		})
+	}
 
 }
 
