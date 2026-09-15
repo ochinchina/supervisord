26c8c3489a05bd9af1eb03320bc0f088cd1baabd
ochinchina <ochinchina@163.com>
2026-09-15 16:06:07 +0800

    unknown field logListenerManager in struct literal of type SysLogger #440

--- logger/log_windows.go
+++ logger/log_windows.go
@@ -4,7 +4,7 @@
 package logger
 
 func NewSysLogger(name string, props map[string]string, logEventEmitter LogEventEmitter) *SysLogger {
-	return &SysLogger{logEventEmitter: logEventEmitter, logWriter: nil, logListenerManager: NewLogListenerManager()}
+	return &SysLogger{logEventEmitter: logEventEmitter, logWriter: nil}
 }
 
 func NewRemoteSysLogger(name string, config string, props map[string]string, logEventEmitter LogEventEmitter) *SysLogger {
